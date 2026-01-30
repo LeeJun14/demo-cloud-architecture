703af94976a315947ef7b92ca6727c22e54b8a11
Leejun14 <777ljy777@gmail.com>
2026-01-30 17:50:36 +0900

    chore: init

---
+++ src/main/java/com/example/democloudarchitecture/DemoCloudArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.democloudarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCloudArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCloudArchitectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/democloudarchitecture/DemoCloudArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.democloudarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCloudArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
