e1af9afe99d7b612d5cd87c2569921d541e26b01
Leejun14 <777ljy777@gmail.com>
2026-02-02 21:21:36 +0900

    test: Exclude AWS auto-configuration in test context

--- src/test/java/com/example/democloudarchitecture/DemoCloudArchitectureApplicationTests.java
+++ src/test/java/com/example/democloudarchitecture/DemoCloudArchitectureApplicationTests.java
@@ -4,12 +4,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @TestPropertySource(properties = {
         "spring.cloud.aws.parameterstore.enabled=false",
-        "spring.cloud.aws.s3.enabled=false"
+        "spring.cloud.aws.s3.enabled=false",
+        "spring.cloud.aws.region.static=ap-northeast-2",
+        "spring.datasource.url=jdbc:h2:mem:testdb",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.autoconfigure.exclude=io.awspring.cloud.autoconfigure.s3.S3AutoConfiguration,io.awspring.cloud.autoconfigure.core.AwsAutoConfiguration"
 })
 class DemoCloudArchitectureApplicationTests {
+
     @Test
     void contextLoads() {
     }
